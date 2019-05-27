86d2f8812e4376e0fabbf785e7ab563d5886bc07
Shannen Wu <shannen@mit.edu>
2019-05-26 23:46:38 -0700

    set up folders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../css/app.css";
 import Route from "react-router-dom/es/Route";
-import Switch from "react-router-dom/es/Switch"
-import Root from "./Root"
+import Switch from "react-router-dom/es/Switch";
+import Root from "./Root";
+import NavBar from "./modules/Navbar.js";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <NavBar
+        />
         <Switch>
           <Route exact path="/" component={Root} />
         </Switch>
---
+++ client/src/components/modules/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>i am a navbar</div>
+        );
+    }
+}
+
+export default NavBar;
