c90ae2478588de5279efa213d86f5bc7266b415b
Shannen Wu <shannen@mit.edu>
2020-04-18 20:56:34 -0400

    casing

--- client/src/components/pages/static/cpw/CpwPhotos.js
+++ client/src/components/pages/static/cpw/CpwPhotos.js
@@ -1,71 +1,71 @@
 const photos = [
   {
-    src: '/cpw_images/0.JPG',
+    src: '/cpw_images/0.jpg',
     height: 2,
     width: 3,
   },
   {
-    src: '/cpw_images/1.JPG',
+    src: '/cpw_images/1.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/3.GIF',
+    src: '/cpw_images/3.gif',
     height: 3,
     width: 2
   },
   {
-    src: '/cpw_images/4.JPG',
+    src: '/cpw_images/4.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/5.JPG',
+    src: '/cpw_images/5.jpg',
     height: 3,
     width: 4
   },
   {
-    src: '/cpw_images/6.JPG',
+    src: '/cpw_images/6.jpg',
     height: 4,
     width: 3
   },
   {
-    src: '/cpw_images/7.JPG',
+    src: '/cpw_images/7.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/9.JPG',
+    src: '/cpw_images/9.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/10.JPG',
+    src: '/cpw_images/10.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/21.JPG',
+    src: '/cpw_images/21.jpg',
     height: 4,
     width: 3
   },
   {
-    src: '/cpw_images/11.JPG',
+    src: '/cpw_images/11.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/12.JPG',
+    src: '/cpw_images/12.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/8.GIF',
+    src: '/cpw_images/8.gif',
     height: 4,
     width: 3
   },
   {
-    src: '/cpw_images/13.JPG',
+    src: '/cpw_images/13.jpg',
     height: 2,
     width: 3
   },
@@ -75,42 +75,42 @@ const photos = [
     width: 3
   },
   {
-    src: '/cpw_images/15.JPG',
+    src: '/cpw_images/15.jpg',
     height: 1,
     width: 2
   },
   {
-    src: '/cpw_images/16.JPG',
+    src: '/cpw_images/16.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/22.JPG',
+    src: '/cpw_images/22.jpg',
     height: 4,
     width: 3
   },
   {
-    src: '/cpw_images/17.JPG',
+    src: '/cpw_images/17.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/20.JPG',
+    src: '/cpw_images/20.jpg',
     height: 3,
     width: 4
   },
   {
-    src: '/cpw_images/23.JPG',
+    src: '/cpw_images/23.jpg',
     height: 4,
     width: 3
   },
   {
-    src: '/cpw_images/18.JPG',
+    src: '/cpw_images/18.jpg',
     height: 2,
     width: 3
   },
   {
-    src: '/cpw_images/19.JPG',
+    src: '/cpw_images/19.jpg',
     height: 2,
     width: 3
   },
