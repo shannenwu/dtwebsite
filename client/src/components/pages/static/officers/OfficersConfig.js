f485bc76cec316b245c7fb1d5684b9aeff9ac955
Shannen Wu <shannen@mit.edu>
2020-09-04 00:16:23 -0400

    update

--- client/src/components/pages/static/officers/OfficersConfig.js
+++ client/src/components/pages/static/officers/OfficersConfig.js
@@ -1,81 +1,81 @@
 const officerOptions = [
   {
-    position: 'President',
-    name: 'Tom Benavides',
-    photoUrl: '/officer_images/tom.jpeg'
+    position: "Show Coordinator",
+    name: "Shannen Wu",
+    photoUrl: "/officer_images/shannen.jpeg",
   },
   {
-    position: 'Vice President',
-    name: 'Xochitl Luna',
-    photoUrl: '/officer_images/xochitl.jpeg'
+    position: "President",
+    name: "Tom Benavides",
+    photoUrl: "/officer_images/tom.jpeg",
   },
   {
-    position: 'Show Coordinator',
-    name: 'Shannen Wu',
-    photoUrl: '/officer_images/shannen.jpeg'
+    position: "Treasurer",
+    name: "Amber Zheng",
+    photoUrl: "/officer_images/amber.jpeg",
   },
   {
-    position: 'Treasurer',
-    name: 'Amber Zheng',
-    photoUrl: '/officer_images/amber.jpeg'
+    position: "Treasurer",
+    name: "Michael Mandanas",
+    photoUrl: "/officer_images/michael.jpeg",
   },
   {
-    position: 'Treasurer',
-    name: 'Michael Mandanas',
-    photoUrl: '/officer_images/michael.jpeg'
+    position: "Pub Chair",
+    name: "Jade Ishii",
+    photoUrl: "/officer_images/jade.jpeg",
   },
   {
-    position: 'Pub Chair',
-    name: 'Jade Ishii',
-    photoUrl: '/officer_images/jade.jpeg'
+    position: "Pub Chair",
+    name: "Jessie Wang",
+    photoUrl: "/officer_images/jessie.jpeg",
   },
   {
-    position: 'Pub Chair',
-    name: 'Jessie Wang',
-    photoUrl: '/officer_images/jessie.jpeg'
+    position: "Social Chair",
+    name: "Avital Vainberg",
+    photoUrl: "/officer_images/avital.jpeg",
   },
   {
-    position: 'Webmaster',
-    name: 'Johan Cervantes',
-    photoUrl: '/officer_images/johan.jpeg'
+    position: "Social Chair",
+    name: "Aniket Dehadrai",
+    photoUrl: "/officer_images/aniket.jpeg",
   },
   {
-    position: 'Secretary',
-    name: 'Juan Aleman',
-    photoUrl: '/officer_images/juan.jpeg'
+    position: "Vice President",
+    name: "Xochitl Luna",
+    photoUrl: "/officer_images/xochitl.jpeg",
   },
   {
-    position: 'Social Chair',
-    name: 'Avital Vainberg',
-    photoUrl: '/officer_images/avital.jpeg'
+    position: "Show Historian",
+    name: "Quentin Thernize",
+    photoUrl: "/officer_images/quentin.jpeg",
   },
   {
-    position: 'Social Chair',
-    name: 'Aniket Dehadrai',
-    photoUrl: '/officer_images/aniket.jpeg'
+    position: "Media Historian",
+    name: "Donald Liu",
+    photoUrl: "/officer_images/donald.jpeg",
   },
   {
-    position: 'Costume Coordinator',
-    name: 'Giulia Alvarenga',
-    photoUrl: '/officer_images/giulia.jpeg'
+    position: "Costume Coordinator",
+    name: "Giulia Alvarenga",
+    photoUrl: "/officer_images/giulia.jpeg",
   },
   {
-    position: 'Show Historian',
-    name: 'Quentin Thernize',
-    photoUrl: '/officer_images/quentin.jpeg'
+    position: "Secretary",
+    name: "Juan Aleman",
+    photoUrl: "/officer_images/juan.jpeg",
   },
   {
-    position: 'Tech Coordinator',
-    name: 'Montse Garza',
-    photoUrl: '/officer_images/montse.jpeg'
+    position: "Webmaster",
+    name: "Johan Cervantes",
+    photoUrl: "/officer_images/johan.jpeg",
   },
   {
-    position: 'Media Historian',
-    name: 'Donald Liu',
-    photoUrl: '/officer_images/donald.jpeg'
+    position: "Tech Coordinator",
+    name: "Montse Garza",
+    photoUrl: "/officer_images/montse.jpeg",
   },
-]
+];
 
 module.exports = {
-  officerOptions
+  officerOptions,
 };
