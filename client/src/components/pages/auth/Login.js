db05389dbbc23ad0ab0e06eec8a24877904dd448
Johan Cervantes <jccamacho19992@gmail.com>
2021-02-25 16:51:11 -0500

    Removed new website message

--- client/src/components/pages/auth/Login.js
+++ client/src/components/pages/auth/Login.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, Redirect } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, Redirect } from "react-router-dom";
 import {
-  Button, Dimmer, Form, Header, Input, Loader, Message
-} from 'semantic-ui-react';
-import axios from 'axios';
-import './auth.css';
+  Button,
+  Dimmer,
+  Form,
+  Header,
+  Input,
+  Loader,
+  Message,
+} from "semantic-ui-react";
+import axios from "axios";
+import "./auth.css";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       errorMsg: [],
       redirect: false,
       loading: false,
@@ -22,32 +28,28 @@ class Login extends React.Component {
 
   static propTypes = {
     loginUser: PropTypes.func.isRequired,
-  }
+  };
 
   handleChange = (e, { name, value }) => {
     this.setState({
       [name]: value,
     });
-  }
+  };
 
   handleSubmit = async (event) => {
     event.preventDefault();
-    const {
-      email,
-      password,
-    } = this.state;
-    const {
-      loginUser,
-    } = this.props;
+    const { email, password } = this.state;
+    const { loginUser } = this.props;
 
     this.setState({
       loading: true,
     });
 
-    axios.post('/login', {
-      email,
-      password,
-    })
+    axios
+      .post("/login", {
+        email,
+        password,
+      })
       .then((response) => {
         loginUser(response.data);
         this.setState({
@@ -78,65 +80,54 @@ class Login extends React.Component {
   };
 
   render() {
-    const {
-      email,
-      password,
-      errorMsg,
-      redirect,
-      loading,
-    } = this.state;
+    const { email, password, errorMsg, redirect, loading } = this.state;
     if (redirect) {
-      return <Redirect to='/profile' />;
+      return <Redirect to="/profile" />;
     }
     return (
-      <div className='login-wrapper'>
+      <div className="login-wrapper">
         <Dimmer active={loading} inverted>
-          <Loader content='Logging you in...' />
+          <Loader content="Logging you in..." />
         </Dimmer>
         <Header
-          as='h1'
-          content='Welcome Back!'
-          style={{ textAlign: 'center' }}
-        />
-        <Message
-          content='This is the new DT website! New and returning dancers should register for a new account.'
-          style={{ textAlign: 'center' }}
+          as="h1"
+          content="Welcome Back!"
+          style={{ textAlign: "center" }}
         />
         <Form>
           <Form.Field>
             <Input
-              name='email'
-              placeholder='Email Address'
+              name="email"
+              placeholder="Email Address"
               onChange={this.handleChange}
               value={email}
             />
           </Form.Field>
           <Form.Field>
             <Input
-              name='password'
-              placeholder='Password'
-              autoComplete='off'
-              type='password'
+              name="password"
+              placeholder="Password"
+              autoComplete="off"
+              type="password"
               onChange={this.handleChange}
               value={password}
             />
           </Form.Field>
-          <Button type='submit' onClick={this.handleSubmit} fluid>
+          <Button type="submit" onClick={this.handleSubmit} fluid>
             Login
           </Button>
         </Form>
         {errorMsg.length !== 0 && (
-          <Message
-            error
-            className='login-error-msg'
-          >
-            <Message.Header content='Error'/>
-            {errorMsg.map((msg, index) => <div key={index}>{msg}</div>)}
+          <Message error className="login-error-msg">
+            <Message.Header content="Error" />
+            {errorMsg.map((msg, index) => (
+              <div key={index}>{msg}</div>
+            ))}
           </Message>
         )}
-        <div className='login-links'>
-          <Link to='/signup'>Register</Link>
-          <Link to='/forgot'>Forgot password?</Link>
+        <div className="login-links">
+          <Link to="/signup">Register</Link>
+          <Link to="/forgot">Forgot password?</Link>
         </div>
       </div>
     );
