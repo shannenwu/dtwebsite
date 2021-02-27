bd0b896a68b5004770a00d0e5d9469c671aab1ce
Johan Cervantes <jccamacho19992@gmail.com>
2021-02-27 11:40:35 -0500

    removed empty event nav bar link

--- client/src/components/modules/Navbar.js
+++ client/src/components/modules/Navbar.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import { Icon } from 'semantic-ui-react';
-import './navbar.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { Icon } from "semantic-ui-react";
+import "./navbar.css";
 
 class NavBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      open: false
+      open: false,
     };
   }
 
@@ -17,93 +17,121 @@ class NavBar extends Component {
     userInfo: PropTypes.object,
     logout: PropTypes.func.isRequired,
     showBg: PropTypes.bool,
-  }
+  };
 
   static defaultProps = {
     userInfo: null,
-    showBg: false
-  }
+    showBg: false,
+  };
 
   toggleMenu = () => {
     const { open } = this.state;
     this.setState({
-      open: !open
+      open: !open,
     });
-  }
+  };
 
   handleItemClick = (path) => {
     this.props.history.push(path);
     this.setState({
-      open: false
-    })
-  }
+      open: false,
+    });
+  };
 
   render() {
     const { open } = this.state;
-    const {
-      userInfo,
-      logout,
-      showBg
-    } = this.props;
+    const { userInfo, logout, showBg } = this.props;
     return (
-      <nav className={showBg ? 'navbar light' : 'navbar dark'}>
-        <span className='navbar-toggle' id='js-navbar-toggle' onClick={() => this.toggleMenu()}>
-          <Icon name={open ? 'times' : 'bars'} />
+      <nav className={showBg ? "navbar light" : "navbar dark"}>
+        <span
+          className="navbar-toggle"
+          id="js-navbar-toggle"
+          onClick={() => this.toggleMenu()}
+        >
+          <Icon name={open ? "times" : "bars"} />
         </span>
-        <div className='logo' onClick={() => this.handleItemClick('/')}>
-          <img src={showBg ? '/site_images/dtlogo-white.png' : '/site_images/dtlogo-black.png'} alt='' />
+        <div className="logo" onClick={() => this.handleItemClick("/")}>
+          <img
+            src={
+              showBg
+                ? "/site_images/dtlogo-white.png"
+                : "/site_images/dtlogo-black.png"
+            }
+            alt=""
+          />
         </div>
-        <ul className={open ? 'main-nav active' : 'main-nav'} id='js-menu'>
+        <ul className={open ? "main-nav active" : "main-nav"} id="js-menu">
           <li>
-            <div className='nav-links' onClick={() => this.handleItemClick('/about')}>
+            <div
+              className="nav-links"
+              onClick={() => this.handleItemClick("/about")}
+            >
               ABOUT
             </div>
           </li>
-          <li>
+          {/* <li>
             <div className='nav-links' onClick={() => this.handleItemClick('/events')}>
               EVENTS
             </div>
-          </li>
+          </li> */}
           <li>
-            <div className='nav-links' onClick={() => this.handleItemClick('/gallery')}>
+            <div
+              className="nav-links"
+              onClick={() => this.handleItemClick("/gallery")}
+            >
               GALLERY
             </div>
           </li>
           <li>
-            <div className='nav-links' onClick={() => this.handleItemClick('/officers')}>
+            <div
+              className="nav-links"
+              onClick={() => this.handleItemClick("/officers")}
+            >
               OFFICERS
             </div>
           </li>
           <li>
-            <div className='nav-links' onClick={() => this.handleItemClick('/auditions')}>
+            <div
+              className="nav-links"
+              onClick={() => this.handleItemClick("/auditions")}
+            >
               AUDITIONS
             </div>
           </li>
           <li>
-            <div className='nav-links' onClick={() => this.handleItemClick('/shows')}>
+            <div
+              className="nav-links"
+              onClick={() => this.handleItemClick("/shows")}
+            >
               SHOWS
             </div>
           </li>
           {userInfo === null ? (
             <li>
-              <div className='nav-links' onClick={() => this.handleItemClick('/login')}>
+              <div
+                className="nav-links"
+                onClick={() => this.handleItemClick("/login")}
+              >
                 LOGIN
               </div>
             </li>
           ) : (
-              <React.Fragment>
-                <li>
-                  <div className='nav-links' onClick={() => this.handleItemClick('/profile')}>
-                    PROFILE
-                  </div>
-                </li>
-                <li>
-                  <div className='nav-links' onClick={logout}>
-                    LOGOUT
-                  </div>
-                </li>
-              </React.Fragment>
-            )}
+            <React.Fragment>
+              <li>
+                <div
+                  className="nav-links"
+                  onClick={() => this.handleItemClick("/profile")}
+                >
+                  PROFILE
+                </div>
+              </li>
+              <li>
+                <div className="nav-links" onClick={logout}>
+                  LOGOUT
+                </div>
+              </li>
+            </React.Fragment>
+          )}
         </ul>
       </nav>
     );
